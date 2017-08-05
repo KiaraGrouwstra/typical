a13daf4d6713507c431131c74fda2928181269cf
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-08-05 15:03:49 +0800

    allow NumberToString to handle number

--- src/cast.spec.ts
+++ src/cast.spec.ts
@@ -5,9 +5,12 @@ import { TestArr } from './fixtures';
 the<'3', NumberToString[3]>();
 the<'3', NumberToString['3']>();
 // the<'3', NumberToString<3>>();
+the<string, NumberToString[number]>();
 
 the<3, StringToNumber['3']>();
 the<3, StringToNumber[3]>();
+the<number, StringToNumber[string]>();
+// ^ fixed by adding fallback `[k: string]: number;`, but causes a bunch of `Type 'StringToNumber[I]' cannot be used to index type ...`
 
 the<"a" | "b" | "c", TupleToUnion<TestArr>>();
 
