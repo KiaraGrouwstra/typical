072aa36c01dcf17699a1a0630592f7c41214df95
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-08-15 03:09:25 +0800

    tsst (wip), new TS. yay less errors!

--- src/util.spec.ts
+++ src/util.spec.ts
@@ -1,8 +1,19 @@
+import 'mocha';
 import { the, If, The, Intersection } from './util';
 
 the<number, 123>();
+// the<string, 123>();
 
 the<123, If<'1', 123, 456>>();
 the<456, If<'0', 123, 456>>();
 
 the<1 & 'a', Intersection<1, 'a'>>();
+
+describe('util', () => {
+  it('better work', () => {
+    the<number, 123>();
+  });
+  it('better asplode', () => {
+    the<string, 123>();
+  });
+});
