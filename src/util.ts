30a8734278f6c1733ebeedbdb4b0ab7802fde5d5
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-02-17 14:16:38 +0100

    borrow types from typezoo
    do I need to include copyrights/licenses? I should figure out what all that means.

--- src/util.ts
+++ src/util.ts
@@ -15,3 +15,28 @@ export declare function the<T, V extends T>(): '1';
 
 // just make an intersection type -- this wrapper helps to beat error `Type ... does not satisfy the constraint ...`
 export type Intersection<A, B> = A & B;
+
+/**
+ * The type of all values; nothing is known about it a priori
+ * except that it exists. The same idea as Flow's `mixed` type.
+ * Borrowed from @pelotom's type-zoo
+ *
+ * @see https://github.com/Microsoft/TypeScript/issues/10715
+ */
+export type unknown = {} | undefined | null;
+
+/**
+ * Use to prevent a usage of type `T` from being inferred in other generics.
+ *
+ * Example:
+ * declare function assertEqual<T>(actual: T, expected: NoInfer<T>): boolean;
+ *
+ * Type `T` will now only be inferred based on the type of the `actual` param, and
+ * the `expected` param is required to be assignable to the type of `actual`.
+ * This allows you to give one particular usage of type `T` full control over how the
+ * compiler infers type `T`.
+ * Borrowed from @pelotom's type-zoo
+ *
+ * @see https://github.com/Microsoft/TypeScript/issues/14829#issuecomment-322267089
+ */
+export type NoInfer<T> = T & { [K in keyof T]: T[K] };
