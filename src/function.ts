d45fe6c596949756e634154c9f1f58d91faa0cf1
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-02-17 23:43:35 +0100

    fix function

--- src/function.ts
+++ src/function.ts
@@ -10,11 +10,12 @@ import { StringToNumber } from './cast';
 /**
  * Get the nth argument of a function.
  */
-export type Argument<N extends StringToNumber[keyof FnShape<any>], T extends FnShape<any>[N]> = T extends FnShape<infer R>[N] ? R: T
-/**
- * helper for `Argument`
- */
-export interface FnShape<N> {
+export type Argument<
+    N extends StringToNumber[keyof FnShape<any>],
+    T extends FnShape<any>[N]
+> = T extends FnShape<infer R>[N] ? R: T;
+
+interface FnShape<N extends number> {
     0: (a0: N, ...args: any[]) => any;
     1: (a0: any, a1: N, ...args: any[]) => any;
     2: (a0: any, a1: any, a2: N, ...args: any[]) => any;
@@ -26,23 +27,25 @@ export interface FnShape<N> {
     8: (a0: any, a1: any, a2: any, a3: any, a4: any, a5: any, a6: any, a7: any, a8: N, ...args: any[]) => any;
     9: (a0: any, a1: any, a2: any, a3: any, a4: any, a5: any, a6: any, a7: any, a8: any, a9: N, ...args: any[]) => any;
     10: (a0: any, a1: any, a2: any, a3: any, a4: any, a5: any, a6: any, a7: any, a8: any, a9: any, a10: N, ...args: any[]) => any;
+    [i: number]: (...args: any[]) => any;
 }
 
 /**
  * Create a function from a tuple of parameter types and a return type.
  */
 export type Fn<A extends any[], R=void> = {
-    0: ()=>R
-    1: (a0:A[0]) => R
-    2: (a0:A[0], a1:A[1]) => R
-    3: (a0:A[0], a1:A[1], a2:A[2]) => R
-    4: (a0:A[0], a1:A[1], a2:A[2], a3:A[3]) => R
-    5: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4]) => R
-    6: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5]) => R
-    7: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5], a6:A[6]) => R
-    8: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5], a6:A[6], a7:A[7]) => R
-    9: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5], a6:A[6], a7:A[7], a8:A[8]) => R
-    10: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5], a6:A[6], a7:A[7], a8:A[8], a9:A[9]) => R
+    0: ()=>R;
+    1: (a0:A[0]) => R;
+    2: (a0:A[0], a1:A[1]) => R;
+    3: (a0:A[0], a1:A[1], a2:A[2]) => R;
+    4: (a0:A[0], a1:A[1], a2:A[2], a3:A[3]) => R;
+    5: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4]) => R;
+    6: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5]) => R;
+    7: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5], a6:A[6]) => R;
+    8: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5], a6:A[6], a7:A[7]) => R;
+    9: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5], a6:A[6], a7:A[7], a8:A[8]) => R;
+    10: (a0:A[0], a1:A[1], a2:A[2], a3:A[3], a4:A[4], a5:A[5], a6:A[6], a7:A[7], a8:A[8], a9:A[9]) => R;
+    [i: number]: (...args: any[]) => R;
 }[The<number, Length<A>>];
 
 /**
