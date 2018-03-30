8edc99438e277daedf9d2a5827c0695b92fa2538
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-03-30 11:21:09 +0200

    IsValidArg, NumberOfArgs

--- src/function.ts
+++ src/function.ts
@@ -8,6 +8,30 @@ import { StringToNumber } from './cast';
  */
 
 /**
+ * Check if an inferred parameter on a function type exists.
+ * @see https://github.com/Microsoft/TypeScript/pull/21316#issuecomment-374191157
+ */
+export type IsValidArg<T> = T extends object ? keyof T extends never ? false : true : true;
+
+/**
+ * Check the number of arguments on a function.
+ * @see https://github.com/Microsoft/TypeScript/pull/21316#issuecomment-374191157
+ */
+export type NumberOfArgs<T extends Function> = 
+    T extends (a: infer A, b: infer B, c: infer C, d: infer D, e: infer E, f: infer F, g: infer G, h: infer H, i: infer I, j: infer J) => any ? (
+        IsValidArg<J> extends true ? 10 :
+        IsValidArg<I> extends true ? 9 :
+        IsValidArg<H> extends true ? 8 :
+        IsValidArg<G> extends true ? 7 :
+        IsValidArg<F> extends true ? 6 :
+        IsValidArg<E> extends true ? 5 :
+        IsValidArg<D> extends true ? 4 :
+        IsValidArg<C> extends true ? 3 :
+        IsValidArg<B> extends true ? 2 :
+        IsValidArg<A> extends true ? 1 : 0
+    ) : 0;
+
+/**
  * Get the nth argument of a function.
  */
 export type Argument<
